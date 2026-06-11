86d4e39fd434ed3104eaf8672b20505a225a66e3
Francisco Rodrigues <f.co.victor@hotmail.com>
2026-06-11 17:53:33 -0300

    chore: some sql files

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/rodriguesfrancisco/chirpy-go/internal/database"
 )
 
 type apiConfig struct {
-	fileserverHits atomic.Int32
+	fileserverHits  atomic.Int32
+	databaseQueries *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
 	const port = ":8080"
 	mux := http.NewServeMux()
 	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
 	apiCfg := apiConfig{}
 	apiCfg.fileserverHits.Store(0)
+	apiCfg.databaseQueries = dbQueries
 	mux.Handle("/app/", apiCfg.middlewareMetricsCounter(handler))
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
